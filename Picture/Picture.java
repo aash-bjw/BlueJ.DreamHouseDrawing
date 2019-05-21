faa81196f6bafbebacfbaf842fb5e47516d9b807
Aashna <bajwa.aashna@gmail.com>
2019-05-21 07:30:47 -0400

    Completed Dream House Lab

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -22,6 +22,7 @@ public class Picture
      */
     public Picture()
     {
+        this.draw();
         // nothing to do... instance variables are automatically set to null
     }
 
@@ -30,29 +31,146 @@ public class Picture
      */
     public void draw()
     {
+        wall = new Square(); //grass
+        wall.changeColor("blue");
+        wall.moveVertical(-50);
+        wall.moveHorizontal(-60);
+        wall.changeSize(300);
+        wall.makeVisible();
+        
+      
+        wall = new Square(); //grass
+        wall.changeColor("green");
+        wall.moveVertical(190);
+        wall.moveHorizontal(-60);
+        wall.changeSize(300);
+        wall.makeVisible();
+        
         wall = new Square();
-        wall.moveVertical(80);
+        wall.changeColor("magenta");
+        wall.moveVertical(100);
+        wall.moveHorizontal(-40);
         wall.changeSize(100);
+        wall.makeVisible();
+        
+        wall = new Square();
+        wall.changeColor("magenta");
+        wall.moveVertical(100);
+        wall.moveHorizontal(40);
+        wall.changeSize(100);
+        wall.makeVisible();
+        
+        wall = new Square(); //outhouse
+        wall.changeColor("red");
+        wall.moveVertical(130);
+        wall.moveHorizontal(160);
+        wall.changeSize(60);
+        wall.makeVisible();
+        
+        wall = new Square(); //outhouse window
+        wall.changeColor("white");
+        wall.moveVertical(140);
+        wall.moveHorizontal(180);
+        wall.changeSize(20);
         wall.makeVisible();
 
         window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
+        window.changeColor("white");
+        window.moveHorizontal(0);
+        window.moveVertical(130);
+        window.makeVisible();
+        
+        window = new Square();
+        window.changeColor("white");
+        window.moveHorizontal(80);
+        window.moveVertical(130);
         window.makeVisible();
 
         roof = new Triangle();
-        roof.changeSize(50, 140);
+        roof.changeColor("black");
+        roof.changeSize(60,190);
         roof.moveHorizontal(60);
-        roof.moveVertical(70);
+        roof.moveVertical(75);
+        roof.makeVisible();
+        
+        roof = new Triangle();
+        roof.changeColor("black");
+        roof.changeSize(50, 70);
+        roof.moveHorizontal(200);
+        roof.moveVertical(120);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treetop 1
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(-10);
+        roof.moveVertical(160);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Tree middle 1
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(-10);
+        roof.moveVertical(180);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treebase 1
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(-10);
+        roof.moveVertical(205);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treetop 2
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(60);
+        roof.moveVertical(160);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Tree middle 2
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(60);
+        roof.moveVertical(180);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treebase 2
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(60);
+        roof.moveVertical(205);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treetop 3
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(130);
+        roof.moveVertical(160);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Tree middle 3
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(130);
+        roof.moveVertical(180);
+        roof.makeVisible();
+        
+        roof = new Triangle(); //Treebase 3
+        roof.changeColor("green");
+        roof.changeSize(30,50);
+        roof.moveHorizontal(130);
+        roof.moveVertical(205);
         roof.makeVisible();
 
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
+        sun.moveHorizontal(170);
+        sun.moveVertical(-20);
         sun.changeSize(60);
         sun.makeVisible();
+        
+       
     }
 
     /**
